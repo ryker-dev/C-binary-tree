00edf6335d90d6267ea73597c77d5e9dc6f12696
Emilio Pizzuti <pizzuti.dev@gmail.com>
2021-12-12 03:35:04 +0200

    Corrected printing, RR and RL

--- binarytree.c
+++ binarytree.c
@@ -4,26 +4,41 @@
 
 typedef struct node {
     int value;
-    short balance;
+    short height;
     struct node *left, *right;
 } node, *tree_pointer;
 
-void add_node(tree_pointer *, int);
-void print_tree(tree_pointer);
-
-int main(int argc, char const *argv[])
+int get_height(node *node)
 {
-    int etp, i = 0, input[] = {2, 4, 6, 8, 10, 12, 14, 28, 30};
-    tree_pointer root = NULL;
-
-    for(i = 0; input[i] != 0; i++) {
-        add_node(&root, input[i]);
-    }
-
-    print_tree(root);
-
-    return 0;
+    if (node == NULL)
+        return 0;
+    return node->height;
 }
+
+
+int calc_balance(node *node)
+{
+    if (node == NULL)
+        return 0;
+    return get_height(node->left) - get_height(node->right);
+}
+
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+/* struct node* create_node(int value)
+{
+    if(!(node* new_node = (tree_pointer)malloc(sizeof(node)))) {
+        perror("Malloc: Failed to reserve memory for a node!");
+        exit(1);
+    }
+    new_node->left = new_node->right = NULL;
+    new_node->height = 1;
+    new_node->value = value;
+    return(new_node);
+} */
 
 void add_node(tree_pointer *parent, int value) {
     // If there is no root (first element)
@@ -34,7 +49,7 @@ void add_node(tree_pointer *parent, int value) {
             exit(1);
         }
         (*parent)->left = (*parent)->right = NULL;
-        (*parent)->balance = 0;
+        (*parent)->height = 1;
         (*parent)->value = value;
     }
 
@@ -47,10 +62,28 @@ void add_node(tree_pointer *parent, int value) {
     }
 }
 
+/* void left_rotate(tree_pointer) {
+
+} */
+
 void print_tree(tree_pointer node)
 {
     if(!node) return;
     print_tree(node->left);
     printf("%d ", node->value);
     print_tree(node->right);
+}
+
+int main(int argc, char const *argv[])
+{
+    int etp, i = 0, input[] = { 2, 4, 6, 8, 10, 12, 14, 28, 30};
+    tree_pointer root = NULL;
+
+    for(i = 0; input[i] != 0; i++) {
+        add_node(&root, input[i]);
+    }
+
+    print_tree(root);
+
+    return 0;
 }
